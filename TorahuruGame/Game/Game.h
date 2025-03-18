a901c310e387a9bcfe3f23c5bff85bba64b4601d
takamatsu haruka <shinharuka1112@gmail.com>
2025-03-19 01:41:40 +0900

    ゲームのレベル追加と特殊床の設置 ステージ改修

--- TorahuruGame/Game/Game.h
+++ TorahuruGame/Game/Game.h
@@ -1,12 +1,12 @@
 #pragma once
 
 #include "Level3DRender/LevelRender.h"
-class Sumaho;
+
 class Player;
 class BackGround;
 class GameCamera;
+class Specialfloor;
 
-class GameClear;
 class Title;
 class Game : public IGameObject
 {
@@ -16,27 +16,23 @@ public:
 	bool Start();
 	void Update();
 	void Render(RenderContext& rc);
-	float m_timer = 120.0f;
+	float                   m_timer = 120.0f;
 	FontRender				m_fontRender;
-	Vector3 m_position;
+	Vector3                 m_position;
 private:
 	Player* m_player = nullptr;
-	
-	GameClear* m_gameClear = nullptr;
 	SoundSource* m_bgm = nullptr;
 	SoundSource* m_se = nullptr;
 	BackGround* m_background = nullptr;
 	ModelRender m_modelRender;
+	LevelRender m_levelRender;//レベル
 	GameCamera* m_gamecamera = nullptr;
-	Sumaho* m_sumaho = nullptr;
 	SkyCube* m_SkyCube = nullptr; //背景
 	int m_skycubeType = enSkyCubeType_NightToon;
 	void InitSky();
 	Vector3 m_pos;
 	//フォントを更新
 	void UpdateFont();
-
-
 	//タイマーの更新
 	void CountTimer();
 };
