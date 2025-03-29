9ac032ebe2176c225e5f2b95176a51f92b08c7e1
takamatsu haruka <shinharuka1112@gmail.com>
2025-03-29 11:22:59 +0900

    クラスが共有されない問題の解決

---
+++ TorahuruGame/Game/Gameover.h
@@ -0,0 +1,18 @@
+#pragma once
+class Game;
+class Player;
+class Title;
+class GameClear;
+
+class Gameover:public IGameObject
+{
+public:
+	Gameover();
+	~Gameover();
+	bool Start();
+	void Update();
+	void Render(RenderContext& rc);
+	Player* m_player;
+	SpriteRender m_spriteRender;
+};
+
