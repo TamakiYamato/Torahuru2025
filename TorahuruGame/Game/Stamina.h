bad1629e16a0af4c9ac669d53db488c6e0917d11
tamakiyamato <CA01244017@st.kawahara.ac.jp>
2025-05-21 20:23:32 +0900

    スタミナバーの左側にダッシュボタンのマークを追加。

--- TorahuruGame/Game/Stamina.h
+++ TorahuruGame/Game/Stamina.h
@@ -17,6 +17,7 @@ private:
 	// 繝｡繝ｳ繝仙､画焚/繧ｯ繝ｩ繧ｹ縺ｧ螳夂ｾｩ縺輔ｌ縺溷､画焚(螻樊ｧ縺ｿ縺溘＞縺ｪ諢溘§)縲・
 	SpriteRender m_spriteRender;
 	SpriteRender m_spriteRender2;
+	SpriteRender m_spriteRender3;
 	Player* m_player;
 };
 
