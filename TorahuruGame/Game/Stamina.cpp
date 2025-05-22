97463f6c0ef3fc5e4e168512c59f4d6a0a0bc37f
TamakiYamato <167502288+TamakiYamato@users.noreply.github.com>
2025-05-22 13:25:36 +0900

    Tamaki/stamina UI (#84)
    * スタミナバーの色を変更、ファイルパスのスペル間違いを修正。
    
    * スタミナバーの左側にダッシュボタンのマークを追加。

--- TorahuruGame/Game/Stamina.cpp
+++ TorahuruGame/Game/Stamina.cpp
@@ -5,12 +5,16 @@
 namespace
 {
 	// スタミナバーの位置。
-	Vector3 SUTAMINA_POSITION = Vector3(0.0f, -400.0f, 0.0f);
-	Vector3 SUTAMINA_POSITION2 = Vector3(-240.0f, -400.0f, 0.0f);
+	Vector3 STAMINA_POSITION = Vector3(80.0f, -400.0f, 0.0f);
+	Vector3 STAMINA_POSITION2 = Vector3(-160.0f, -400.0f, 0.0f);
+	// ダッシュボタンのマークの位置。
+	Vector3 STAMINABUTTON_POSITION = Vector3(-240.0f, -405.0f, 0.0f);
 
 	// スタミナバーの大きさ。
-	Vector3 SUTAMINA_SCALE = Vector3(1.0f, 1.0f, 1.0f);
-	Vector3 SUTAMINA_SCALE2 = Vector3(0.96f, 0.85f, 0.85f);
+	Vector3 STAMINA_SCALE = Vector3(1.0f, 1.0f, 1.0f);
+	Vector3 STAMINA_SCALE2 = Vector3(0.96f, 0.85f, 0.85f);
+	// ダッシュボタンのマークの位置。
+	Vector3 STAMINABUTTON_SCALE = Vector3(0.8f, 3.0f, 1.0f);
 
 	// ピボット。(右端原点)
 	Vector2 SET_PIVOT = Vector2(0.0f, 0.5f);
@@ -33,22 +37,28 @@ bool Stamina::Start()
 {
 	// 画像を読み込む。
 	// スタミナバー(枠組み)の画像。
-	m_spriteRender.Init("Assets/PlayerUI/sutaminaBar.DDS", 500.0f, 100.0f);
+	m_spriteRender.Init("Assets/PlayerUI/staminaBar.DDS", 500.0f, 100.0f);
 	// スタミナバー(減らす部分)の画像。
-	m_spriteRender2.Init("Assets/PlayerUI/sutamina.DDS", 500.0f, 100.0f);
+	m_spriteRender2.Init("Assets/PlayerUI/stamina.DDS", 500.0f, 100.0f);
+	// ダッシュボタンのマーク。
+	m_spriteRender3.Init("Assets/PlayerUI/staminaButton.DDS", 500.0f, 100.0f);
 
 	// playerを探す。→探さないとnullptr判定になる。
 	m_player = FindGO<Player>("player");
 
 	// スタミナバー(枠組)の位置。
-	m_spriteRender.SetPosition(Vector3(SUTAMINA_POSITION));
+	m_spriteRender.SetPosition(Vector3(STAMINA_POSITION));
 	// スタミナバー(減らす部分)の位置。
-	m_spriteRender2.SetPosition(Vector3(SUTAMINA_POSITION2));
+	m_spriteRender2.SetPosition(Vector3(STAMINA_POSITION2));
+	// ダッシュボタンのマークの位置。
+	m_spriteRender3.SetPosition(Vector3(STAMINABUTTON_POSITION));
 
 	// スタミナバー(枠組み)の大きさ。
-	m_spriteRender.SetScale(Vector3(SUTAMINA_SCALE));
+	m_spriteRender.SetScale(Vector3(STAMINA_SCALE));
 	// スタミナバー(減らす部分)の大きさ。
-	m_spriteRender2.SetScale(Vector3(SUTAMINA_SCALE2));
+	m_spriteRender2.SetScale(Vector3(STAMINA_SCALE2));
+	// ダッシュボタンのマークの大きさ。
+	m_spriteRender3.SetScale(Vector3(STAMINABUTTON_SCALE));
 
 	// ピボットの位置。
 	m_spriteRender2.SetPivot(Vector2(SET_PIVOT));
@@ -64,7 +74,7 @@ void Stamina::StaminaCalc()
 		float newVitality = m_player->m_stamina;
 		// 割合を計算→scaleのX値に掛け算→バーが縮小する。
 		float wari = (float)newVitality / (float)MaxVitality;
-		Vector3 scal = { SUTAMINA_SCALE2 };
+		Vector3 scal = { STAMINA_SCALE2 };
 		// 割合の計算結果を代入。
 		// 割合の応じて横幅だけ縮める。
 		scal.x *= wari;
@@ -78,6 +88,7 @@ void Stamina::Update()
 	// スプライトを更新。
 	m_spriteRender.Update();
 	m_spriteRender2.Update();
+	m_spriteRender3.Update();
 }
 
 void Stamina::Render(RenderContext& rc)
@@ -85,4 +96,5 @@ void Stamina::Render(RenderContext& rc)
 	// 画像を描画。
 	m_spriteRender.Draw(rc);
 	m_spriteRender2.Draw(rc);
+	m_spriteRender3.Draw(rc);
 }
--- TorahuruGame/Game/main.cpp
+++ TorahuruGame/Game/main.cpp
@@ -36,7 +36,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
 
 	//Game繧ｯ繝ｩ繧ｹ縺ｮ繧ｪ繝悶ず繧ｧ繧ｯ繝医ｒ菴懈・縲・
 	NewGO<Title>(0, "title");
-	NewGO<Loading>(0, "loading");
+	NewGO<Loading>(1, "loading");
 	
 	//////////////////////////////////////
 	// 蛻晄悄蛹悶ｒ陦後≧繧ｳ繝ｼ繝峨ｒ譖ｸ縺上・縺ｯ縺薙％縺ｾ縺ｧ・・ｼ・ｼ・
