929547b62cd1594af7eeff7e97a73124987fdb81
takamatsu haruka <shinharuka1112@gmail.com>
2025-07-15 13:54:12 +0900

    Stairsのリファクタリングを行いました

--- TorahuruGame/Game/Stairs.cpp
+++ TorahuruGame/Game/Stairs.cpp
@@ -4,34 +4,33 @@
 #include"Player.h"
 #include"GameClear.h"
 #include"FirstFloor.h"
+
 namespace
 {
-	const Vector3 COLLISION_HEIGHT = Vector3(0.0f, 150.0f, 0.0f);
-	const Vector3 COLLISION_SIZE = Vector3(500.0f, 500.0f, 500.0f);//コリジョンの大きさ
+	const Vector3 COLLISION_HEIGHT = Vector3(0.0f, 150.0f, 0.0f);    // 当たり判定の大きさ
+	const Vector3 COLLISION_SIZE = Vector3(500.0f, 500.0f, 500.0f); // コリジョンの大きさ
+}
 
-}
-Stairs::Stairs() {
+bool Stairs::Start()
+{
+	m_modelRender.Init("Assets/modelData/Stage1/Stairs/Stairs.tkm");
 
-}
-Stairs::~Stairs() {
-	
-}
-bool Stairs::Start() {
-	m_modelRender.Init("Assets/modelData/Stage1/Stairs/Stairs.tkm");	//髫取ｮｵ繧ｪ繝悶ず繧ｧ繧ｯ繝・
-	
-	//繝励Ξ繧､繝､繝ｼ蛛ｴ縺ｮ繧ｪ繝悶ず繧ｧ繧ｯ繝医ｒ謖√▲縺ｦ縺上ｋ
 	m_player = FindGO<Player>("player");
+
 	m_collisionObject = NewGO<CollisionObject>(0, "collisionObject");
+
+	// コリジョンの当たり範囲の設定
 	m_collisionObject->CreateBox(
-		m_position + COLLISION_HEIGHT,	//座標
-		Quaternion::Identity,			//回転
-		COLLISION_SIZE					//大きさ
+		m_position + COLLISION_HEIGHT,	// 階段の座標
+		Quaternion::Identity,			// 階段の回転
+		COLLISION_SIZE					// 階段の大きさ
 	);
+
 	return true;
 }
 
-void Stairs::Update() {
-
+void Stairs::Update()
+{
 	m_modelRender.SetPosition(m_position);
 	
 	m_modelRender.Update();	
